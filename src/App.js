27767cba33a9bbae5dc9d5b509661f9247736935
Leonardo Gassen <leo22re@gmail.com>
2023-12-18 14:09:40 -0300

    Projeto Estruturado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+// Reaproveitamento de estrutura
+import { Link, Outlet } from "react-router-dom";
+import Container from "./components/layout/Container";
+
 function App() {
   return (
     <div className="App">
-      <h1>Costs</h1>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </ul>
+      <Container customClass="min-height">
+        <Outlet />
+      </Container>
+
+      <p>Footer</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,43 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// 1 - Configurando o router:
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Company from "./routes/Company";
+import Contact from "./routes/Contact";
+import ErrorPage from "./routes/ErrorPage";
+import Home from "./routes/Home";
+import NewProject from "./routes/NewProject";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/company",
+        element: <Company />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/newproject",
+        element: <NewProject />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
