7643f4b94db0d0f16a87032a86a0a9a6fc21c9ce
Leonardo Gassen <leo22re@gmail.com>
2023-12-22 10:30:27 -0300

    Criado arquivo json e conectada com API

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 import styles from "./ProjectForm.module.css";
 
 function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <form className={styles.form}>
       <Input
@@ -19,7 +34,11 @@ function ProjectForm({ btnText }) {
         text="Orçamento do projeto"
       />
       <div>
-        <Select name="category_id" text="Selecione a categoria" />
+        <Select
+          name="category_id"
+          text="Selecione a categoria"
+          options={categories}
+        />
       </div>
       <SubmitButton text={btnText} />
     </form>
